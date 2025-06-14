1dd5b5262f87ea5f10d407accbf72f6755bf71a3
1538379200 <1538379200@qq.com>
2025-06-14 11:04:39 +0800

    update avante config for different os

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,9 +1,20 @@
+local function is_win() return vim.fn.has "win32" == 1 end
+
+local function get_avante_fields()
+  if is_win() then
+    return { mode = "legacy", disable_tools = true }
+  else
+    return { mode = "agentic", disable_tools = false }
+  end
+end
+
 return {
   "yetone/avante.nvim",
   opts = function(_, opts)
+    local fields_table = get_avante_fields()
     return require("astrocore").extend_tbl(opts, {
       -- windows 使用其他形式，并且不使用工具
-      mode = "legacy",
+      mode = fields_table.mode,
       provider = "deepseek",
       providers = {
         deepseek = {
@@ -11,7 +22,7 @@ return {
           api_key_name = "DEEPSEEK_API_KEY",
           endpoint = "https://api.deepseek.com",
           model = "deepseek-coder",
-          disable_tools = true,
+          disable_tools = fields_table.disable_tools,
         },
       },
     })
