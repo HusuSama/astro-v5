3a51744551325199bf3e5a9494b54029708bfd50
1538379200 <1538379200@qq.com>
2025-06-14 10:57:35 +0800

    update avante config

--- lua/plugins/avante.lua
+++ lua/plugins/avante.lua
@@ -1,14 +1,19 @@
 return {
   "yetone/avante.nvim",
-  opts = {
-    provider = "deepseek",
-    vendors = {
-      deepseek = {
-        __inherited_from = "openai",
-        api_key_name = "DEEPSEEK_API_KEY",
-        endpoint = "https://api.deepseek.com",
-        model = "deepseek-coder",
+  opts = function(_, opts)
+    return require("astrocore").extend_tbl(opts, {
+      -- windows 使用其他形式，并且不使用工具
+      mode = "legacy",
+      provider = "deepseek",
+      providers = {
+        deepseek = {
+          __inherited_from = "openai",
+          api_key_name = "DEEPSEEK_API_KEY",
+          endpoint = "https://api.deepseek.com",
+          model = "deepseek-coder",
+          disable_tools = true,
+        },
       },
-    },
-  },
+    })
+  end,
 }
