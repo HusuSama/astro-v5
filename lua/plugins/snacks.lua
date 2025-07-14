8a9c470d741b065cad07fa43a0413b35be89620d
1538379200 <1538379200@qq.com>
2025-07-14 21:39:02 +0800

    增加自定义snacks中lsp跳转方式和显示方式

--- lua/plugins/snacks.lua
+++ lua/plugins/snacks.lua
@@ -2,6 +2,7 @@ return {
   {
     "folke/snacks.nvim",
     opts = {
+      ---@type snacks.indent.Config
       indent = {
         enabled = true,
         chunk = {
@@ -19,20 +20,5 @@ return {
         },
       },
     },
-  },
-  {
-    "AstroNvim/astrolsp",
-    ---@param opts AstroLSPOpts
-    opts = function(_, opts)
-      if opts.mappings.n["<Leader>lR"] then
-        opts.mappings.n["<Leader>lR"][1] = function()
-          require("snacks.picker").lsp_references {
-            auto_confirm = false,
-            auto_close = true,
-            layout = { preset = "telescope" },
-          }
-        end
-      end
-    end,
   },
 }
